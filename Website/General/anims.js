03725b9d77371606f50173999672037036c80c46
Endrreman <nagy.endre.bulcsu@gmail.com>
2024-04-14 13:00:25 +0200

    04.14_3.0
    header anims

--- Website/General/anims.js
+++ Website/General/anims.js
@@ -2,10 +2,10 @@
 ScrollOut({
   targets: "#hr_load",
   onShown(el) {
-      el.classList.add("hr_load");
+    el.classList.add("hr_load");
   },
   onHidden(el) {
-      el.classList.remove("hr_load");
+    el.classList.remove("hr_load");
   }
 });
 
@@ -13,9 +13,45 @@ ScrollOut({
 ScrollOut({
   targets: ".foot_grid",
   onShown(el) {
-      el.classList.add("footer_load");
+    el.classList.add("footer_load");
   },
   onHidden(el) {
     el.classList.remove("footer_load");
+  }
+});
+
+var root = document.documentElement;
+
+ScrollOut({ 
+  targets: "#h_h1",
+  onShown(el) {
+    root.style.setProperty('--t-delay', '.2s');
+    el.classList.add("y0","d1");
+  },
+  onHidden(el) {
+    root.style.setProperty('--t-delay', '0s');
+    el.classList.remove("y0","d1");
+  }
+});
+ScrollOut({
+  targets: "#h_hr_load",
+  onShown(el) {
+    root.style.setProperty('--t-delay', '1s');
+    el.classList.add("h_hr_load","d1");
+  },
+  onHidden(el) {
+    root.style.setProperty('--t-delay', '0s');
+    el.classList.remove("h_hr_load","d1");
+  }
+});
+ScrollOut({
+  targets: "#h_h2",
+  onShown(el) {
+    root.style.setProperty('--t-delay', '1.5s');
+    el.classList.add("y0","d1");
+  },
+  onHidden(el) {
+    root.style.setProperty('--t-delay', '0s');
+    el.classList.remove("y0","d1");
   }
 });
