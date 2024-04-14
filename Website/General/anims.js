874bad8b34bc407453d64c52194d10e7f268214c
Endrreman <nagy.endre.bulcsu@gmail.com>
2024-04-14 13:17:55 +0200

    04.14_3.1
    footer fix
    anim fix?

--- Website/General/anims.js
+++ Website/General/anims.js
@@ -26,32 +26,32 @@ ScrollOut({
   targets: "#h_h1",
   onShown(el) {
     root.style.setProperty('--t-delay', '.2s');
-    el.classList.add("y0","d1");
+    el.classList.add("d1","y0");
   },
   onHidden(el) {
     root.style.setProperty('--t-delay', '0s');
-    el.classList.remove("y0","d1");
+    el.classList.remove("d1","y0");
   }
 });
 ScrollOut({
   targets: "#h_hr_load",
   onShown(el) {
     root.style.setProperty('--t-delay', '1s');
-    el.classList.add("h_hr_load","d1");
+    el.classList.add("d1","h_hr_load");
   },
   onHidden(el) {
     root.style.setProperty('--t-delay', '0s');
-    el.classList.remove("h_hr_load","d1");
+    el.classList.remove("d1","h_hr_load");
   }
 });
 ScrollOut({
   targets: "#h_h2",
   onShown(el) {
     root.style.setProperty('--t-delay', '1.5s');
-    el.classList.add("y0","d1");
+    el.classList.add("d1","y0");
   },
   onHidden(el) {
     root.style.setProperty('--t-delay', '0s');
-    el.classList.remove("y0","d1");
+    el.classList.remove("d1","y0");
   }
 });
